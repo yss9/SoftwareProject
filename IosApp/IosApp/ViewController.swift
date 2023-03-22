921b1fbc57dd9bb5f05918580ac4671fd2f38b51
yss9 <yse2196@gmail.com>
2023-03-23 04:14:01 +0900

    first commit

---
+++ IosApp/IosApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IosApp
+//
+//  Created by 서영석 on 2023/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblHello: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
